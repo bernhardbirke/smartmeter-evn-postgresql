f727d3c7f012e221281dbb2d3b6683f0bd3e6ca8
Bernhard Birke <archer0730@gmx.at>
2023-05-18 20:15:07 +0200

    implementing EVN update from  2.Mai2023

--- EVNSmartmeter-postgreSQL.py
+++ EVNSmartmeter-postgreSQL.py
@@ -1,189 +1,295 @@
-import serial
-import time
-from cryptography.hazmat.primitives.ciphers.aead import AESGCM
-from binascii import unhexlify
+import json
 import sys
-import string
+import os
+import serial
+from datetime import datetime
+from binascii import unhexlify
 from gurux_dlms.GXDLMSTranslator import GXDLMSTranslator
+from gurux_dlms.TranslatorOutputType import TranslatorOutputType
 from bs4 import BeautifulSoup
 from Cryptodome.Cipher import AES
 from time import sleep
-from gurux_dlms.TranslatorOutputType import TranslatorOutputType
-# load postgreSQL database information as a module
-from postgresql_tasks import insert_smartmeter
-# load environment variables (evn_schluessel)
-import os
+import xml.etree.ElementTree as ET
+import time
 from dotenv import load_dotenv
 load_dotenv()
 
+
+#Aktuellen Dateipfad finden und mit config.json erweitern
+configFile = os.path.dirname(os.path.realpath(__file__)) + '/config.json' 
+
+# Überprüfung ob ein Config Datei vorhanden ist sonst kommt eine Fehlermeldung und beendet das Programm
+if not os.path.exists(configFile):
+    print("Kein Configfile gefunden bitte eines anlegen.")
+    sys.exit(-1)
+
+# Überprüfung ob die Config gelesen werden kann
+if not os.access(configFile, os.R_OK):
+    print("ConfigFile " + configFile + " kann nicht gelesen werden!\n\n")
+    sys.exit(-2)
+
+# Einlesen der Config
+config = json.load(open(configFile))
+
+# Überprüfung ob alle Daten in der Config vorhanden sind
+neededConfig = ['port', 'baudrate', 'printValue', 'useMQTT', 'mqttbrokerip', 'mqttbrokerport', 'mqttbrokeruser', 'mqttbrokerpasswort', 'useInfluxdb', 'influxdbip', 'influxdbport']
+for conf in neededConfig:
+    if conf not in config:
+        print(conf + ' Fehlt im Configfile!')
+        sys.exit(3)
+
+
 # EVN Schlüssel eingeben zB. "36C66639E48A8CA4D6BC8B282A793BBB"
-evn_schluessel = os.getenv("EVN_SCHLUESSEL")
+key = os.getenv("EVN_SCHLUESSEL")
 
-# PostgreSQL Verwenden (True | False)
-usePostgreSQL = True
+#Aktulle Werte auf Console ausgeben (True | False)
+printValue = config['printValue']
 
-# Comport Config/Init
-comport = "/dev/ttyUSB0"
-
-# Aktulle Werte auf Console ausgeben (True | False)
-printValue = False
-
-
-# Holt Daten von serieller Schnittstelle
-def recv(serialIncoming):
-    while True:
-        data = serialIncoming.read_all()
-        if data == '':
-            continue
-        else:
-            break
-        sleep(0.5)
-    return data
-
-# Konvertiert Signed Ints
+#MQTT Verwenden (True | False) und Grundeinstellungen
+useMQTT = config['useMQTT']
+mqttBroker = config['mqttbrokerip']
+mqttport = config['mqttbrokerport']             #1883 ist der Standard Port
+mqttuser =config['mqttbrokeruser']              #wenn kein User verwendet wird leer lassen ""
+mqttpasswort = config['mqttbrokerpasswort']     #wenn kein Passwort verwendet wird leer lassen ""
 
 
-def s16(value):
-    val = int(value, 16)
-    return -(val & 0x8000) | (val & 0x7fff)
+#Comport Config/Init
+comport = config['port']
+
+#InfluxDB Config/init
+useinfluxdb = config['useInfluxdb']
+influxdbhost = config['influxdbip']
+influxdbport = config['influxdbport']
+influxdbdatenbank = 'SmartMeter'
+
+tr = GXDLMSTranslator()
+ser = serial.Serial( port=comport,
+         baudrate=2400,
+         bytesize=serial.EIGHTBITS,
+         parity=serial.PARITY_NONE,
+         stopbits=serial.STOPBITS_ONE
+)
+
+#MQTT Init
+if useMQTT:
+    import paho.mqtt.client as mqtt
+    try:
+        client = mqtt.Client("SmartMeter")
+        client.username_pw_set(mqttuser, mqttpasswort)
+        client.connect(mqttBroker, mqttport)
+    except:
+        print("Die Ip Adresse des Brokers ist falsch!")
+        sys.exit()
+
+if useinfluxdb:
+    from influxdb import InfluxDBClient
+    try:
+        clientinfluxdb = InfluxDBClient(host=influxdbhost, port=influxdbport, database=influxdbdatenbank)
+    except Exception as err:
+        print("Kann nicht mit InfluxDB verbinden!")
+        print()
+        print("Fehler: ", format(err))
+        sys.exit()
+    
+
+# Werte im XML File
+octet_string_values = {}
+octet_string_values['0100010800FF'] = 'WirkenergieP'
+octet_string_values['0100020800FF'] = 'WirkenergieN'
+octet_string_values['0100010700FF'] = 'MomentanleistungP'
+octet_string_values['0100020700FF'] = 'MomentanleistungN'
+octet_string_values['0100200700FF'] = 'SpannungL1'
+octet_string_values['0100340700FF'] = 'SpannungL2'
+octet_string_values['0100480700FF'] = 'SpannungL3'
+octet_string_values['01001F0700FF'] = 'StromL1'
+octet_string_values['0100330700FF'] = 'StromL2'
+octet_string_values['0100470700FF'] = 'StromL3'
+octet_string_values['01000D0700FF'] = 'Leistungsfaktor'
 
 
-def s8(value):
-    val = int(value, 16)
-    return -(val & 0x80) | (val & 0x7f)
-
-
-# DLMS Blue Book Page 52
-# https://www.dlms.com/files/Blue_Book_Edition_13-Excerpt.pdf
-units = {
-    27: "W",  # 0x1b
-    30: "Wh",  # 0x1e
-    33: "A",  # 0x21
-    35: "V",  # 0x23
-    255: ""  # 0xff: no unit, unitless
-}
-
-
-tr = GXDLMSTranslator(TranslatorOutputType.SIMPLE_XML)
-serIn = serial.Serial(port=comport,
-                      baudrate=2400,
-                      bytesize=serial.EIGHTBITS,
-                      parity=serial.PARITY_NONE,
-                      stopbits=serial.STOPBITS_ONE
-                      )
-
-
-while True:
-    sleep(14.7)
-    daten = recv(serIn)
-    if daten != '':
-        daten = daten.hex()
-    if (daten == '' or daten[0:8] != "68010168"):
-        print(f"Invalid Start Bytes... waiting at {time.ctime()}")
-        continue
-    systemTitel = daten[22:38]
-    frameCounter = daten[44:52]
-    frame = daten[52:560]
-
+def evn_decrypt(frame, key, systemTitel, frameCounter):
     frame = unhexlify(frame)
-    encryption_key = unhexlify(evn_schluessel)
+    encryption_key = unhexlify(key)
     init_vector = unhexlify(systemTitel + frameCounter)
     cipher = AES.new(encryption_key, AES.MODE_GCM, nonce=init_vector)
-    apdu = cipher.decrypt(frame).hex()
+    return cipher.decrypt(frame).hex()
 
+while True:
+    daten = ser.read(size=282).hex()    
+    mbusstart = daten[0:8]
+    frameLen=int("0x" + mbusstart[2:4],16)
+    systemTitel = daten[22:38]
+    frameCounter = daten[44:52]
+    frame = daten[52:12+frameLen*2]
+    if mbusstart[0:2] == "68" and mbusstart[2:4] == mbusstart[4:6] and mbusstart[6:8] == "68" :
+        print("Daten ok")
+    else:
+        print(f"wrong M-Bus Start, restarting at {time.ctime()}")
+        sleep(2.5)
+        ser.flushOutput()
+        ser.close()
+        ser.open()
+
+    apdu = evn_decrypt(frame,key,systemTitel,frameCounter)
+    if apdu[0:4] != "0f80" :
+        continue
     try:
         xml = tr.pduToXml(apdu,)
-        soup = BeautifulSoup(xml, 'lxml')
-        results_32 = soup.find_all('uint32')
-        results_16 = soup.find_all('uint16')
-        results_int16 = soup.find_all('int16')
-        results_int8 = soup.find_all('int8')
-        results_enum = soup.find_all('enum')
+        #print("xml: ",xml)
 
+        root = ET.fromstring(xml)
+        found_lines = []
+        momentan = []
+
+        items = list(root.iter())
+        for i, child in enumerate(items):
+            if child.tag == 'OctetString' and 'Value' in child.attrib:
+                value = child.attrib['Value']
+                if value in octet_string_values.keys():
+                    if ('Value' in items[i+1].attrib):
+                        if value in ['0100010700FF', '0100020700FF']:
+                            # special handling for momentanleistung
+                            momentan.append(int(items[i+1].attrib['Value'], 16))
+                        found_lines.append({'key': octet_string_values[value], 'value': int(items[i+1].attrib['Value'], 16)});
+
+#        print(found_lines)
     except BaseException as err:
+        #print("APU: ", format(apdu))
         print("Fehler: ", format(err))
-        continue
+        continue;
 
     try:
-        # Wirkenergie A+ in Wattstunden
-        WirkenergieP = int(str(results_32[0].get(
-            'value')), 16)*10**s8(str(results_int8[0].get('value')))
-        WirkenergiePUnit = units[int(results_enum[0].get('value'), 16)]
+        if len(momentan) == 2:
+            found_lines.append({'key': 'Momentanleistung', 'value': momentan[0]-momentan[1]})
 
-        # Wirkenergie A- in Wattstunden
-        WirkenergieN = int(str(results_32[1].get(
-            'value')), 16)*10**s8(str(results_int8[1].get('value')))
-        WirkenergieNUnit = units[int(results_enum[1].get('value'), 16)]
+        for element in found_lines:
 
-        # Momentanleistung P+ in Watt
-        MomentanleistungP = int(str(results_32[2].get(
-            'value')), 16)*10**s8(str(results_int8[2].get('value')))
-        MomentanleistungPUnit = units[int(results_enum[2].get('value'), 16)]
+            if element['key'] == "WirkenergieP":
+               WirkenergieP = element['value']/1000
+            if element['key'] == "WirkenergieN":
+               WirkenergieN = element['value']/1000
 
-        # Momentanleistung P- in Watt
-        MomentanleistungN = int(str(results_32[3].get(
-            'value')), 16)*10**s8(str(results_int8[3].get('value')))
-        MomentanleistungNUnit = units[int(results_enum[3].get('value'), 16)]
+            if element['key'] == "MomentanleistungP":
+               MomentanleistungP = element['value']
+            if element['key'] == "MomentanleistungN":
+               MomentanleistungN = element['value']
 
-        # Spannung L1
-        SpannungL1 = int(str(results_16[0].get(
-            'value')), 16)*10**s8(str(results_int8[4].get('value')))
-        SpannungL1Unit = units[int(results_enum[4].get('value'), 16)]
+            if element['key'] == "SpannungL1":
+               SpannungL1 = element['value']*0.1
+            if element['key'] == "SpannungL2":
+               SpannungL2 = element['value']*0.1
+            if element['key'] == "SpannungL3":
+               SpannungL3 = element['value']*0.1
 
-        # Spannung L2
-        SpannungL2 = int(str(results_16[1].get(
-            'value')), 16)*10**s8(str(results_int8[5].get('value')))
-        SpannungL2Unit = units[int(results_enum[5].get('value'), 16)]
+            if element['key'] == "StromL1":
+               StromL1 = element['value']*0.01
+            if element['key'] == "StromL2":
+               StromL2 = element['value']*0.01
+            if element['key'] == "StromL3":
+               StromL3 = element['value']*0.01
 
-        # Spannung L3
-        SpannungL3 = int(str(results_16[2].get(
-            'value')), 16)*10**s8(str(results_int8[6].get('value')))
-        SpannungL3Unit = units[int(results_enum[6].get('value'), 16)]
-
-        # Strom L1
-        StromL1 = int(str(results_16[3].get('value')), 16) * \
-            10**s8(str(results_int8[7].get('value')))
-        StromL1Unit = units[int(results_enum[7].get('value'), 16)]
-
-        # Strom L2
-        StromL2 = int(str(results_16[4].get('value')), 16) * \
-            10**s8(str(results_int8[8].get('value')))
-        StromL2Unit = units[int(results_enum[8].get('value'), 16)]
-
-        # Strom L3
-        StromL3 = int(str(results_16[5].get('value')), 16) * \
-            10**s8(str(results_int8[9].get('value')))
-        StromL3Unit = units[int(results_enum[9].get('value'), 16)]
-
-        # Leistungsfaktor
-        Leistungsfaktor = s16(str(results_int16[0].get(
-            'value')))*10**s8(str(results_int8[10].get('value')))
-        LeistungsfaktorUnit = units[int(results_enum[10].get('value'), 16)]
-
-        if printValue:
-            print('Wirkenergie+: ' + str(WirkenergieP) + WirkenergiePUnit)
-            print('Wirkenergie-: ' + str(WirkenergieN) + WirkenergieNUnit)
-            print('Momentanleistung+: ' +
-                  str(MomentanleistungP) + MomentanleistungPUnit)
-            print('Momentanleistung-: ' +
-                  str(MomentanleistungN) + MomentanleistungNUnit)
-            print('Spannung L1: ' + str(SpannungL1) + SpannungL1Unit)
-            print('Spannung L2: ' + str(SpannungL2) + SpannungL2Unit)
-            print('Spannung L3: ' + str(SpannungL3) + SpannungL3Unit)
-            print('Strom L1: ' + str(StromL1) + StromL1Unit)
-            print('Strom L2: ' + str(StromL2) + StromL2Unit)
-            print('Strom L3: ' + str(StromL3) + StromL3Unit)
-            print('Leistungsfaktor: ' + str(Leistungsfaktor) + LeistungsfaktorUnit)
-            print('Momentanleistung: ' + str(MomentanleistungP -
-                  MomentanleistungN) + MomentanleistungPUnit)
-            print()
-            print()
-
-        # PostgreSQL
-        if usePostgreSQL:
-            insert_smartmeter(WirkenergieP, WirkenergieN, MomentanleistungP, MomentanleistungN,
-                              SpannungL1, SpannungL2, SpannungL3, StromL1, StromL2, StromL3, Leistungsfaktor)
+            if element['key'] == "Leistungsfaktor":
+               Leistungsfaktor = element['value']*0.001
 
     except BaseException as err:
         print("Fehler: ", format(err))
-        continue
+        continue;    
+    
+
+    #MQTT
+    if useMQTT:
+        connected = False
+        while not connected:
+            try:
+                client.reconnect()
+                connected = True
+            except:
+                print("Lost Connection to MQTT...Trying to reconnect in 2 Seconds")
+                time.sleep(2)
+                
+
+    if printValue:
+        now = datetime.now()
+        print("\n\t\t*** KUNDENSCHNITTSTELLE ***\n\nOBIS Code\tBezeichnung\t\t\t Wert")
+        print(now.strftime("%d.%m.%Y %H:%M:%S"))
+        print("1.0.32.7.0.255\tSpannung L1 (V):\t\t "+ str(round(SpannungL1,2)))
+        print("1.0.52.7.0.255\tSpannung L2 (V):\t\t "+ str(round(SpannungL2,2)))
+        print("1.0.72.7.0.255\tSpannung L3 (V):\t\t "+ str(round(SpannungL3,2)))
+        print("1.0.31.7.0.255\tStrom L1 (A):\t\t\t "+ str(round(StromL1,2)))
+        print("1.0.51.7.0.255\tStrom L2 (A):\t\t\t "+ str(round(StromL2,2)))
+        print("1.0.71.7.0.255\tStrom L3 (A):\t\t\t "+ str(round(StromL3,2)))
+        print("1.0.1.7.0.255\tWirkleistung Bezug [W]: \t "+str(MomentanleistungP))
+        print("1.0.2.7.0.255\tWirkleistung Lieferung [W]:\t "+str(MomentanleistungN))
+        print("1.0.1.8.0.255\tWirkenergie Bezug [kWh]:\t "+str(WirkenergieP))
+        print("1.0.2.8.0.255\tWirkenergie Lieferung [kWh]:\t "+str(WirkenergieN))
+        print("-------------\tLeistungsfaktor:\t\t "+str(Leistungsfaktor))
+        print("-------------\tWirkleistunggesamt [w]:\t\t " + str(MomentanleistungP-MomentanleistungN))
+        
+    #MQTT
+    if useMQTT:
+        client.publish("Smartmeter/WirkenergieBezug",WirkenergieP)
+        client.publish("Smartmeter/WirkenergieLieferung",WirkenergieN)
+        client.publish("Smartmeter/WirkleistungBezug",MomentanleistungP)
+        client.publish("Smartmeter/WirkleistungLieferung",MomentanleistungN)
+        client.publish("Smartmeter/Wirkleistunggesamt",MomentanleistungP - MomentanleistungN)
+        client.publish("Smartmeter/SpannungL1",SpannungL1)
+        client.publish("Smartmeter/SpannungL2",SpannungL2)
+        client.publish("Smartmeter/SpannungL3",SpannungL3)
+        client.publish("Smartmeter/StromL1",StromL1)
+        client.publish("Smartmeter/StromL2",StromL2)
+        client.publish("Smartmeter/StromL3",StromL3)
+        client.publish("Smartmeter/Leistungsfaktor",Leistungsfaktor)
+    try:
+        if useinfluxdb:
+            mytime = int(time.time()*1000000000)
+            json_body = [
+            {
+                "measurement": "Wirkenergie",
+                "fields": {
+                    "Bezug": WirkenergieP,
+                    "Lieferung": WirkenergieN
+                },
+                "time": mytime
+            },
+            {
+                "measurement": "Momentanleistung",
+                "fields": {
+                    "Bezug": MomentanleistungP,
+                    "Lieferung": MomentanleistungN,
+                    "Gesamt": MomentanleistungP-MomentanleistungN
+                },
+                "time": mytime
+            },
+            {
+                "measurement": "Spannung",
+                "fields": {
+                    "L1": SpannungL1,
+                    "L2": SpannungL2,
+                    "L3": SpannungL3,
+                },
+                "time": mytime
+            },
+            {
+                "measurement": "Strom",
+                "fields": {
+                    "L1": StromL1,
+                    "L2": StromL2,
+                    "L3": StromL3,
+                },
+                "time": mytime
+            },
+            {
+                "measurement": "Leistungsfaktor",
+                "fields": {
+                    "value": Leistungsfaktor
+                },
+                "time": mytime
+            }
+            ]
+            clientinfluxdb.write_points(json_body,database=influxdbdatenbank)
+    except BaseException as err:
+        print("Es ist ein Fehler aufgetreten.")
+        print()
+        print("Fehler: ", format(err))
+        sys.exit()
+
